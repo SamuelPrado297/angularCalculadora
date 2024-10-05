0c26a8b56d69282e31ec67805b7aed8168238675
SamuelPrado297 <samueldasilvaprado@gmail.com>
2024-10-05 14:33:55 -0300

    Initial commit

---
+++ src/app/teclado/teclado.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teclado',
+  templateUrl: './teclado.component.html',
+  styleUrl: './teclado.component.css'
+})
+export class TecladoComponent implements OnInit {
+
+  constructor() {}
+
+  public resultado:string = "";
+
+  clicar(valor:string) {
+    this.resultado+=valor;
+  }
+  limpar() {
+    this.resultado = "";
+  }
+  finalizar() {
+    this.resultado = eval(this.resultado).toString();
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
